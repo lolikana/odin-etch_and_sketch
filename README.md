d003fccda723a55044f3c5e3a75342f6714c20ed
Lolikana <thomas.guiart@gmail.com>
2022-04-22 09:01:00 +0900

    custom slider range, add draw function

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 Second javascript project: HTML, CSS, JS
 
 Memo :
-- [ ] none
+- [ ] setup custom step for range slide (step = actual value**2)
 
 Extra :
 - [ ] none
--- script.js
+++ script.js
@@ -1,38 +1,106 @@
-// feedback current value Pixel Size
+const canvasSize = 512;
+const square = document.querySelector('#square');
 
-const pixelSize = document.querySelector('#pixel-size');
-const output = document.querySelector('.pixel-output');
-
-output.textContent = pixelSize.value;
-
-pixelSize.addEventListener('input', function() {
-  output.textContent = pixelSize.value;
-});
-
-// 16x16 grid
-const sixteenBtn = document.querySelector('#sixteen-btn');
-sixteenBtn.addEventListener('click', sixteenDiv);
-
-function sixteenDiv() {
-  for (let i = 0; i < 256; i++) {
-    const square = document.querySelector('#square');
+const createElementDiv = (sizeValue) => {
+  for (let i = 0; i < (sizeValue * sizeValue); i++) {
     const createDiv = document.createElement('div');
-    createDiv.style.width = '32px';
-    createDiv.style.height = '32px';
-    square.appendChild(createDiv)[i]
+    createDiv.style.width = canvasSize / sizeValue + 'px';
+    createDiv.style.height = canvasSize / sizeValue + 'px';
+    createDiv.classList.add('square-div');
+    square.appendChild(createDiv)[i];
   }
+
+  changeBackgroundOnClick();
 }
 
-// 32x32 button
-const thirtyTwoBtn = document.querySelector('#thirty-two-btn');
-thirtyTwoBtn.addEventListener('click', thirtyTwoDiv);
+// default size
 
-function thirtyTwoDiv() {
-  for (let i = 0; i < 1024; i++) {
-    const square = document.querySelector('#square');
-    const createDiv = document.createElement('div');
-    createDiv.style.width = '16px';
-    createDiv.style.height = '16px';
-    square.appendChild(createDiv)[i]
+createElementDiv(4)
+
+// Active btn reset canvas // 
+
+const selectClearBtn = document.getElementById('reset-btn');
+
+selectClearBtn.addEventListener('click', function() {
+  document.getElementById('square').textContent = '';
+
+  createElementDiv(8);
+})
+
+// Select size btn and clear canvas and create div //
+
+const selectSizeBtn = document.getElementsByClassName('size-btn');
+
+for (let i = 0; i < selectSizeBtn.length; i++) {
+    selectSizeBtn[i].addEventListener('click', setSizeDiv = () => {
+    const selectSizeValue = document.getElementsByClassName('size-span')[i].innerHTML;
+    
+    document.getElementById('square').textContent = '';
+    
+    createElementDiv(selectSizeValue);
+  })
+};
+
+// Range slider update size //
+
+// // const rangePixelSize = document.querySelector('#range-size');
+// // const output = document.querySelector('.range-output');
+
+// // output.textContent = rangePixelSize.value + ' x ' + rangePixelSize.value;
+
+// // rangePixelSize.addEventListener('input', function() {
+// //   const getRangeValue = rangePixelSize.value;
+// //   output.textContent = rangePixelSize.value + ' x ' + rangePixelSize.value;
+
+// //   document.getElementById('square').textContent = '';
+  
+// //   createElementDiv(getRangeValue);
+// // });
+
+// Custom slider range step update size //
+const rangePixelSize = document.querySelector('#range-size');
+const output = document.querySelector('.range-output');
+
+let customRangeValues = [4, 8, 16, 32, 64];
+
+rangePixelSize.oninput = function(e){
+  output.textContent = customRangeValues[this.value]+ ' x ' + customRangeValues[this.value];
+  rangePixelSize.max = customRangeValues.length - 1;
+};
+
+rangePixelSize.oninput()
+
+rangePixelSize.addEventListener('input', function() {
+  const getRangeValue = customRangeValues[this.value];
+
+  document.getElementById('square').textContent = '';
+
+  createElementDiv(getRangeValue);
+});
+
+// change background div when clicked // 
+let colorTrigger = false 
+
+square.addEventListener('mousedown', () => {      
+    colorTrigger = true;  
+});   
+
+square.addEventListener('mouseup', () => {      
+    colorTrigger = false;  
+});
+
+function changeBackgroundOnClick() {
+  const getSquareDiv = document.getElementsByClassName('square-div');
+  
+  for (let i = 0; i < getSquareDiv.length; i++) {
+    getSquareDiv[i].addEventListener('mousedown', setBackground = () => {
+        getSquareDiv[i].style.backgroundColor = 'red';
+    });
+
+    getSquareDiv[i].addEventListener('mousemove', setBackground = () => {
+      if (colorTrigger === true ) {
+        getSquareDiv[i].style.backgroundColor = 'black';
+      }
+    });   
   }
 }
